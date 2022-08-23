81d0984b6a70c85c0a99b39783380819ca20b4d1
LeventSoykan <levent.soykan@turkcell.com.tr>
2022-08-23 21:41:09 +0300

    Weather API Integrated - OpenWeather

--- precip.py
+++ precip.py
@@ -1,2 +1,17 @@
+import os
+import requests
+import json
 
-print('hi')
+
+api_key = os.environ.get('OPEN_WEATHER_API')
+latitude = 40.911753
+longitude = 29.251945
+
+source = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={api_key}')
+
+forecast_json = json.loads(source.text)
+
+forecast_str = json.dumps(forecast_json, indent=4, sort_keys=True)
+print(forecast_str)
+for day in forecast_json['list']:
+    print(f"Date: {day['dt_txt']} - Weather: {day['weather'][0]['description']}")
