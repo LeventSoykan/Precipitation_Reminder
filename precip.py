af822c9501a4020aad567b298caf705e79f07924
LeventSoykan <levent.soykan@turkcell.com.tr>
2022-08-24 22:03:32 +0300

    Update subject and content of reminder mail for next day only

--- precip.py
+++ precip.py
@@ -3,17 +3,20 @@ import requests
 import json
 import smtplib, ssl
 import email.message
-
+from datetime import datetime, timedelta
 
 def get_forecast(latitude, longitude, api_key):
     source = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={api_key}')
     forecast_json = json.loads(source.text)
     forecast = ''
+    tomorrow = (datetime.today()+timedelta(days=1)).date()
     for day in forecast_json['list']:
-        forecast += f"Date: {day['dt_txt']} - Weather: {day['weather'][0]['description']}\n"
-    return forecast
+        day_datetime = datetime.strptime(day['dt_txt'], '%Y-%m-%d %H:%M:%S')
+        if day_datetime.date() == tomorrow:
+            forecast += f"Date: {day['dt_txt']} - Weather: {day['weather'][0]['description']}\n"
+    return tomorrow, forecast
 
-def send_forecast_email(port, sender_email, password, receiver_email, message):
+def send_forecast_email(port, sender_email, password, receiver_email, message, date):
     context = ssl.create_default_context()
     with smtplib.SMTP("smtp.gmail.com", port) as server:
         server.ehlo()  # Can be omitted
@@ -23,7 +26,7 @@ def send_forecast_email(port, sender_email, password, receiver_email, message):
         m = email.message.Message()
         m['From'] = sender_email
         m['To'] = receiver_email
-        m['Subject'] = "Weather Forecast"
+        m['Subject'] = f"Weather Forecast for {date}"
         m.set_payload(message)
         server.sendmail(sender_email, receiver_email, m.as_string())
 
@@ -36,5 +39,5 @@ if __name__ == '__main__':
     port = 587  # For SSL
     sender_email = "leventsoykan83@gmail.com"
     receiver_email = "levent_soykan@yahoo.com"
-    message= get_forecast(latitude, longitude, api_key)
-    send_forecast_email(port, sender_email, gmail_pass, receiver_email, message)
+    date, message = get_forecast(latitude, longitude, api_key)
+    send_forecast_email(port, sender_email, gmail_pass, receiver_email, message, date)
